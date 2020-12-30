824b47483912a43deb250c4bafe50535fedd87ce
Tushar Bhatnagar <40886260+tushar-31093@users.noreply.github.com>
2020-12-30 13:10:25 +0530

    Add files via upload

---
+++ utils/client_api.py
@@ -0,0 +1,10 @@
+import requests
+import json
+
+json_file = 'utils/api_data.json'
+url = 'http://localhost:5000/detect'
+with open(json_file) as f:
+    data = json.load(f)
+
+server_return = requests.post(url, json=data)
+print(server_return)
